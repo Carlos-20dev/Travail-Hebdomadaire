3ef79de5cf332bab34873b6ce1066862a4dad2cd
Carlos-20dev <74969974+Carlos-20dev@users.noreply.github.com>
2021-08-05 13:18:38 +0200

    Add files via upload

---
+++ Cours Javascript/script.js
@@ -0,0 +1,69 @@
+/*var x = 12;
+var y = 5
+// var result = x/y;
+// x++;(veut dire x = x + 1)
+
+console.log(x);
+*/
+
+/*
+var x= 5;
+var y = 12;
+
+// var myBoolean = (x>3 && y<15) ;
+var myBoolean = (x>3 || y<10) ;
+
+*********************
+console.log(true && true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false);
+console.log("--------------");
+console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
+console.log("---------------");
+console.log(!true);
+console.log(!false);
+-------------------------------*/
+
+/*var speed = 70;
+
+if(speed<80)
+{
+    if(speed<50)
+    {
+        console.log("Acceleres un petit peu");
+    }
+    else
+    {
+        console.log("Tu roules a la bonne vitesse");
+
+    }
+    
+}
+else if(speed>100)
+{
+    console.log("Il faut que tu ralentisses un petit peu");
+}
+else
+{
+    console.log("tu roules beaucoup trop vite,cest dangereux!");
+}
+*/
+
+
+var favoriteColor = blue;
+
+switch(favoriteColor)
+{
+    case "blue":
+        console.log("wouah: le bleu c'est trop beau");
+        break;
+    case "red":
+        console.log("J'aimerais bien une voiture rouge!");
+        break;   
+     default:
+}
+
